5ec759c8ecc364736731687d09036678f17c0d55
Peter Benjamin <peter_benjamin@intuit.com>
2016-05-20 01:04:12 -0700

    New landing page for devsecops.github.io

---
+++ js/main.js
@@ -0,0 +1,97 @@
+var app = angular.module('site', ['ui.bootstrap', 'ngAria']);
+
+app.factory('Backend', ['$http',
+    function($http) {
+      var get = function(url) {
+        return function() {
+          return $http.get(url)
+            .then(function(resp) {
+              return resp.data;
+            });
+        };
+      };
+
+      return {
+        bootcamp: get('data/bootcamp.json'),
+        projects: get('data/projects.json')
+      };
+    }
+  ])
+  .controller('MainCtrl', ['Backend', '$scope', 'filterFilter', '$anchorScroll',
+    function(Backend, $scope, filterFilter, $anchorScroll) {
+      var self = this;
+
+      Backend.projects()
+        .then(function(data) {
+          self.projects = data;
+        });
+
+      Backend.bootcamp()
+        .then(function(data) {
+          self.bootcamp = data;
+
+          $.ajax({
+            url: 'https://raw.githubusercontent.com/devsecops/devsecops.github.io/master/data/bootcamp.json',
+            dataType: 'jsonp',
+            jsonpCallback: 'JSON_CALLBACK',
+            success: function(data) {
+              var projects = data[0].AllProjects;
+              $scope.currentPage = 1; //current page
+              $scope.maxSize = 5; //pagination max size
+              $scope.entryLimit = 36; //max rows for data table
+
+              /* init pagination with $scope.list */
+              $scope.noOfRepos = projects.length;
+              $scope.noOfPages = Math.ceil($scope.noOfRepos / $scope.entryLimit);
+              $scope.resultsSectionTitle = 'All Repos';
+              $scope.pageChanged = function() {
+                $anchorScroll();
+              };
+
+              $scope.$watch('searchText', function(term) {
+                // Create $scope.filtered and then calculate $scope.noOfPages, no racing!
+                $scope.filtered = filterFilter(projects, term);
+                $scope.noOfRepos = $scope.filtered.length;
+                $scope.noOfPages = Math.ceil($scope.noOfRepos / $scope.entryLimit);
+                $scope.resultsSectionTitle = (!term) ? 'All Repos' : (($scope.noOfRepos === 0) ? 'Search results' : ($scope.noOfRepos + ' repositories found'));
+              });
+
+              var featuredProjects = new Array();
+
+              self.featured.forEach(function(name) {
+                for (var i = 0; i < projects.length; i++) {
+                  var project = projects[i];
+                  if (project.Name == name) {
+                    featuredProjects.push(project);
+                    return;
+                  }
+                }
+              });
+
+              self.projects = projects;
+              self.featuredProjects = featuredProjects;
+              $scope.$apply();
+            }
+          });
+          $.ajax({
+            url: 'https://popularrepostg.blob.core.windows.net/popularrepos/projectssummary.json',
+            dataType: 'jsonp',
+            jsonpCallback: 'JSON_CALLBACK',
+            success: function(stats) {
+              if (stats !== null) {
+                $scope.overAllStats = stats[0];
+              }
+            }
+          });
+        });
+    }
+  ])
+  .filter('startFrom', function() {
+    return function(input, start) {
+      if (input) {
+        start = +start; //parse to int
+        return input.slice(start);
+      }
+      return [];
+    };
+  });
